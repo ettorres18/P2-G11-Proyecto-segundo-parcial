36172db0bbcbdb16e16f1cc487b20d188b8c3cc7
RobertAlec <77252823+RobertAlec@users.noreply.github.com>
2021-01-13 22:04:33 -0500

    Add files via upload

---
+++ Juego.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.pooespol.proyectop2g11;
+import java.util.*;
+
+/**
+ *
+ * @author alecs
+ */
+public class Juego {
+    private String fecha;
+    private int duracion;
+    private ArrayList<Jugador> Jugadores;
+    private Jugador ganador;
+    private Configuracion configuracion;
+    private Mazo mazo;
+    private ArrayList<Carta> cartasJugadas;
+    private Alineacion alineacion;
+    
+    public reificarJuego(){
+    }
+}
