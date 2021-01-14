6efc747ea4f7d7059da65451d14a9f1f92350033
RobertAlec <77252823+RobertAlec@users.noreply.github.com>
2021-01-13 21:03:31 -0600

    Update CARTA.java

--- CARTA.java
+++ CARTA.java
@@ -4,12 +4,29 @@
  * and open the template in the editor.
  */
 
-package tarea1;
+package com.pooespol.proyectop2g11;
+import java.util.*;
 
 /**
  *
  * @author alecs
  */
 public class CARTA {
+    private int numero;
+    private String nombre;
+    private String rutaImagen;
+    
+    public Producto (int numero, String nombre, String rutaImagen) {
+        this.numero = numero;
+        this.nombre = nombre;
+        this.rutaImagen = rutaImagen;
+    }
 
+    public equals (Object) {
+        if(.equals()){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
