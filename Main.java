cd97f786fe25081aa6c3651715cc30add64bb88e
Ceres <74085453+ettorres18@users.noreply.github.com>
2021-01-14 10:29:16 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,24 +4,21 @@
  * and open the template in the editor.
  */
 
-
-import Reporte.java;
-
 /**
  *
  * @author alecs
  */
-class Main{
-public ArrayList<Reporte> reporte;
+public class Main{
+    public ArrayList<Reporte> reporte;
 
-public Main(){
-reporte = new ArrayList<>();
-}
-
-public void mostrarReporte(){
-    for (Reporte r:reporte){
-        System.out.print("----Fecha------Duracion-----Ganador----Oponentes-----Alineacion-----");
-        System.out.print("   "+r.getFecha(),+"    "+r.getDuracion,+"    "+r.getGanador,+"   "+r.getJugadores,+"   "+r.getAlineacion)
+    public Main(){
+     reporte = new ArrayList<>();
     }
-}
+
+    public void mostrarReporte(){
+        for (Reporte r:reporte){
+         System.out.print("----Fecha------Duracion-----Ganador----Oponentes-----Alineacion-----");
+         System.out.print("   "+r.getFecha(),+"    "+r.getDuracion(),+"    "+r.getGanador(),+"   "+r.getJugadores(),+"   "+r.getAlineacion())
+        }
+    }
 }
