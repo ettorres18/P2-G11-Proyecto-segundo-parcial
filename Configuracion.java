af5e02ec4ef99c25708ff3e2761b982dbcc2e623
RobertAlec <77252823+RobertAlec@users.noreply.github.com>
2021-01-14 10:12:50 -0500

    Add files via upload

---
+++ Configuracion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.pooespol.proyectop2g11;
+import java.util.*;
+
+/**
+ *
+ * @author alecs
+ */
+public class Configuracion {
+    private boolean visible;
+    private int numOponentes;
+    
+
+    public Configuracion(boolean visible, int numOponentes){
+        this.visible = visible;
+        this.numOponentes = numOponentes
+    }
+    public boolean isVisible(){
+        return visible;
+    }
+    
+    public void setVisible(boolean visible){
+        this.visible = visible;
+    }
+}
