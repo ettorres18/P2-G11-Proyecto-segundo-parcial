05ebcb965910ed443abd0c60ac8d1222e994caf0
RobertAlec <77252823+RobertAlec@users.noreply.github.com>
2021-01-13 23:55:33 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,12 +4,24 @@
  * and open the template in the editor.
  */
 
-package tarea1;
+
+import Reporte.java;
 
 /**
  *
  * @author alecs
  */
-public class Main {
+class Main{
+public ArrayList<Reporte> reporte;
 
+public Main(){
+reporte = new ArrayList<>();
+}
+
+public void mostrarReporte(){
+    for (Reporte r:reporte){
+        System.out.print("----Fecha------Duracion-----Ganador----Oponentes-----Alineacion-----");
+        System.out.print("   "+r.getFecha(),+"    "+r.getDuracion,+"    "+r.getGanador,+"   "+r.getJugadores,+"   "+r.getAlineacion)
+    }
+}
 }
