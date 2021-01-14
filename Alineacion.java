e62a7745786c138a2725c2cd9bb211f6c79397d8
Ceres <74085453+ettorres18@users.noreply.github.com>
2021-01-14 10:41:25 -0500

    Update Alineacion.java

--- Alineacion.java
+++ Alineacion.java
@@ -4,11 +4,20 @@
  * and open the template in the editor.
  */
 package com.pooespol.clases;
+import java.util.*;
 
 /**
  *
  * @author Torres Jara
  */
 public class Alineacion {
+    private ArrayList<String> ubicacion;
+    private ArrayList<String> nombre;
+    
+    public voidseleccionarAlineacion(){
+        Random random = new Random();
+        int indice= random.nextInt(ubicacion.size());
+        System.out.println(nombre.get(indice));
+    }
     
 }
